ff7f8fd0423e5dff8d181d7ec7ac1520cfe6133d
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-05-18 20:23:26 +0200

    dd

--- public/script.js
+++ public/script.js
@@ -27,33 +27,14 @@ var peer = new Peer({
   port: window.location.port || (window.location.protocol === 'https:' ? 443 : 80),
   path: '/peerjs',
   config: {
-    'iceServers': [
-      { url: 'stun:stun01.sipphone.com' },
-      { url: 'stun:stun.ekiga.net' },
-      { url: 'stun:stunserver.org' },
-      { url: 'stun:stun.softjoys.com' },
-      { url: 'stun:stun.voiparound.com' },
-      { url: 'stun:stun.voipbuster.com' },
-      { url: 'stun:stun.voipstunt.com' },
-      { url: 'stun:stun.voxgratia.org' },
-      { url: 'stun:stun.xten.com' },
+    iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
-      {
-        url: 'turn:192.158.29.39:3478?transport=udp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      },
-      {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      }
-    ]
+    ],
   },
-
   debug: 3
 });
+
 
 peer.on("call", (call) => {
   call.answer(myVideoStream);
