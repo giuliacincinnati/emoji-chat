bcf792da44f649853fcc6ec1766be89f871f4aef
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-05-19 10:25:18 +0200

    super roll back

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ const showChat = document.querySelector("#showChat");
 const backBtn = document.querySelector(".header__back");
 myVideo.muted = true;
 
+
 backBtn.addEventListener("click", () => {
   document.querySelector(".main__left").style.display = "flex";
   document.querySelector(".main__left").style.flex = "1";
@@ -21,10 +22,28 @@ showChat.addEventListener("click", () => {
 
 const user = prompt("Enter your name");
 
-var peer = new Peer(undefined, {
-  host: "/",
-  port: "3001",
+var peer = new Peer({
+  host: window.location.hostname,
+  port: window.location.port || (window.location.protocol === 'https:' ? 443 : 80),
+  path: '/peerjs',
+  config: {
+    iceServers: [
+      { urls: 'stun:stun.l.google.com:19302' },
+      { urls: 'stun:stun1.l.google.com:19302' },
+    ],
+  },
+  debug: 3
 });
+
+
+peer.on("call", (call) => {
+  call.answer(myVideoStream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+});
+
 
 let myVideoStream;
 navigator.mediaDevices
@@ -37,7 +56,7 @@ navigator.mediaDevices
     addVideoStream(myVideo, stream);
 
     peer.on("call", (call) => {
-      console.log("someone is calling");
+      console.log('someone call me');
       call.answer(stream);
       const video = document.createElement("video");
       call.on("stream", (userVideoStream) => {
@@ -50,19 +69,21 @@ navigator.mediaDevices
     });
   });
 
+  const connectToNewUser = (userId, stream) => {
+    console.log('I call someone' + userId);
+    const call = peer.call(userId, myVideoStream); // Passa myVideoStream invece di stream
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
+  };
+
+
+
 peer.on("open", (id) => {
-  console.log("My ID is: " + id);
+  console.log('my id is' + id);
   socket.emit("join-room", ROOM_ID, id, user);
 });
-
-const connectToNewUser = (userId, stream) => {
-  console.log("I'm calling someone: " + userId);
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
@@ -77,14 +98,16 @@ let send = document.getElementById("send");
 let messages = document.querySelector(".messages");
 let currentEmotion = "";
 
+
+
 send.addEventListener("click", (e) => {
   if (text.value.length !== 0) {
     if (text.value.includes("felice")) {
       currentEmotion = "felice";
     } else {
-      currentEmotion = "";
+      currentEmotion = ""; // Se il messaggio non contiene la parola "felice", reimposta lo stato emotivo corrente
     }
-    updateEmoticon();
+    updateEmoticon(); // Aggiorna l'immagine dell'emoticon
     socket.emit("message", text.value);
     text.value = "";
   }
@@ -95,28 +118,32 @@ text.addEventListener("keydown", (e) => {
     if (text.value.includes("felice")) {
       currentEmotion = "felice";
     } else {
-      currentEmotion = "";
+      currentEmotion = ""; // Se il messaggio non contiene la parola "felice", reimposta lo stato emotivo corrente
     }
-    updateEmoticon();
+    updateEmoticon(); // Aggiorna l'immagine dell'emoticon
     socket.emit("message", text.value);
     text.value = "";
   }
 });
 
+
+
 socket.on("createMessage", (message, userName) => {
   let messageContent = message;
+
   if (message.includes("felice")) {
     const emoticonImage = `<img src="felice.png" alt="Emoticon">`;
     messageContent = `${messageContent} ${emoticonImage}`;
   }
+
   messages.innerHTML += `
     <div class="message">
-      <b><i class="far fa-user-circle"></i> <span>${
-        userName === user ? "me" : userName
-      }</span></b>
+      <b><i class="far fa-user-circle"></i> <span>${userName === user ? "me" : userName}</span></b>
       <span>${messageContent}</span>
     </div>`;
 });
+
+
 
 function updateEmoticon() {
   if (currentEmotion === "felice") {
@@ -133,15 +160,19 @@ function createEmoticon() {
   emoticonImage.style.top = "50%";
   emoticonImage.style.transform = "translate(-50%, -50%)";
   document.body.appendChild(emoticonImage);
+
+  // Scomparsa della faccina dopo 10 secondi
   setTimeout(() => {
     document.body.removeChild(emoticonImage);
   }, 10000);
-};
+}
+
+
+
 
 const inviteButton = document.querySelector("#inviteButton");
 const muteButton = document.querySelector("#muteButton");
 const stopVideo = document.querySelector("#stopVideo");
-
 muteButton.addEventListener("click", () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
