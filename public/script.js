01ff68370682ebd50c4a23514d45802368e8ec50
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-05-23 21:27:15 +0200

    s

--- public/script.js
+++ public/script.js
@@ -4,6 +4,9 @@ const myVideo = document.createElement("video");
 const showChat = document.querySelector("#showChat");
 const backBtn = document.querySelector(".header__back");
 myVideo.muted = true;
+const myVideoEmoticon = document.createElement("img");
+myVideoEmoticon.id = "emoticon-image";
+
 
 backBtn.addEventListener("click", () => {
   document.querySelector(".main__left").style.display = "flex";
@@ -77,8 +80,10 @@ const addVideoStream = (video, stream) => {
   video.addEventListener("loadedmetadata", () => {
     video.play();
     videoGrid.append(video);
+    videoGrid.appendChild(myVideoEmoticon); // Aggiungi l'elemento dell'immagine dell'emoticon al videoGrid
   });
 };
+
 
 let text = document.querySelector("#chat_message");
 let send = document.getElementById("send");
@@ -144,18 +149,21 @@ socket.on("createMessage", (message, userName) => {
 
   if (includeEmoticon) {
     updateEmoticon();
+    myVideoEmoticon.style.display = userName === user ? "block" : "none"; // Mostra l'immagine dell'emoticon sul riquadro video del mittente
   }
 });
 
+
 function updateEmoticon() {
   if (currentEmotion === "felice") {
-    createEmoticon("felice.png");
+    myVideoEmoticon.src = "felice.png";
   } else if (currentEmotion === "triste") {
-    createEmoticon("triste.png");
+    myVideoEmoticon.src = "triste.png";
   } else if (currentEmotion === "arrabbiato") {
-    createEmoticon("arrabbiato.png");
+    myVideoEmoticon.src = "arrabbiato.png";
   }
 }
+
 
 function createEmoticon(imageFileName) {
   const emoticonImage = document.createElement("img");
