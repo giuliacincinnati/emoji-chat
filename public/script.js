53d1aeb428732c390afc61b57dd71edf26c5e392
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-05-24 15:19:50 +0200

    dddd

--- public/script.js
+++ public/script.js
@@ -59,26 +59,32 @@ navigator.mediaDevices
     });
   });
 
-const connectToNewUser = (userId, stream) => {
-  console.log('I call someone' + userId);
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-};
+  const connectToNewUser = (userId, stream) => {
+    console.log('I call someone' + userId);
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+      addVideoStream(video, userVideoStream, userId); // Passa l'ID del mittente come parametro
+    });
+  };
+
 
 peer.on("open", (id) => {
   console.log('my id is' + id);
   socket.emit("join-room", ROOM_ID, id, user);
 });
 
-const addVideoStream = (video, stream) => {
+const addVideoStream = (video, stream, sender) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
     videoGrid.appendChild(video);
-    videoGrid.appendChild(emoticonContainer); // Aggiungi emoticonContainer come figlio di videoGrid
+    if (sender === peer.id) {
+    videoGrid.appendChild(emoticonContainer); // Aggiungi emoticonContainer al riquadro del mittente
+  } else {
+    video.parentNode.appendChild(emoticonContainer); // Aggiungi emoticonContainer al riquadro del destinatario
+  }
+};
   });
 };
 
