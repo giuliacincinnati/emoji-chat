1e4454c6595d8feeb16e80ae3552104d88337b5e
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-05-25 10:00:40 +0200

    RRR

--- public/script.js
+++ public/script.js
@@ -79,7 +79,7 @@ const addVideoStream = (video, stream) => {
   video.addEventListener("loadedmetadata", () => {
     video.play();
     videoGrid.append(video);
-    videoGrid.appendChild(emoticonContainer); // Aggiungi emoticonContainer come figlio di videoGrid
+    video.append(emoticonContainer); // Aggiungi emoticonContainer come figlio di videoGrid
   });
 };
 
