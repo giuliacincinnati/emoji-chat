d4102b269d1c637abefc98e43a2f029bd328b2e4
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-06-01 22:33:52 +0200

    505

--- public/script.js
+++ public/script.js
@@ -56,14 +56,19 @@ navigator.mediaDevices
     });
 
 
-socket.on("user-connected", (userId) => {
-  connectToNewUser(userId, myVideoStream);
-  if (userEmotions[userId]) {
-    updateEmoticonContainer(userId);
-  }
-   updateEmoticonContainer(peer.id);
-});
-});
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, myVideoStream);
+
+      // Invia l'evento "user-emotion" per tutti gli utenti connessi
+      Object.entries(userEmotions).forEach(([connectedUserId, emotion]) => {
+        socket.emit("user-emotion", connectedUserId, emotion);
+      });
+
+      // Invia l'evento "user-emotion" per l'utente corrente
+      if (currentEmotion !== "") {
+        socket.emit("user-emotion", peer.id, currentEmotion);
+      }
+    });
 
 const connectToNewUser = (userId, stream) => {
   console.log('I call someone' + userId);
@@ -159,15 +164,13 @@ text.addEventListener("keydown", (e) => {
 });
 
 socket.on("user-emotion", (userId, emotion) => {
-  //userEmotions[userId] = emotion;
-//  updateEmoticonImage(userId);
   if (userId === peer.id) {
     currentEmotion = emotion;
-  updateEmoticonContainer(peer.id);
-} else {
-  userEmotions[userId] = emotion;
- updateEmoticonContainer(userId);
-}
+    updateEmoticonContainer(peer.id);
+  } else {
+    userEmotions[userId] = emotion;
+    updateEmoticonContainer(userId);
+  }
 });
 
 
@@ -176,16 +179,11 @@ socket.on("createMessage", (message, userName, userId) => {
   let includeEmoticon = false;
 
   if (message.emotion === "felice") {
-    currentEmotion = "felice";
     includeEmoticon = true;
   } else if (message.emotion === "arrabbiato") {
-    currentEmotion = "arrabbiato";
     includeEmoticon = true;
   } else if (message.emotion === "triste") {
-    currentEmotion = "triste";
     includeEmoticon = true;
-  } else {
-    currentEmotion = "";
   }
 
   messages.innerHTML += `
@@ -195,10 +193,10 @@ socket.on("createMessage", (message, userName, userId) => {
     </div>`;
 
   if (includeEmoticon) {
+    userEmotions[userId] = message.emotion;
     updateEmoticonContainer(userId);
   }
 });
-
 
 
 
