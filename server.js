42248ad26bb43a0c8e1e09648e4b68fe8cec978a
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-06-05 19:15:57 +0200

    dai

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
-    origin: '*'
-  }
+    origin: "*",
+  },
 });
 const { ExpressPeerServer } = require("peer");
 const opinions = {
   debug: true,
-}
+};
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
@@ -24,21 +24,19 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-// Memorizza lo stato dell'emoticon per ogni utente
-const userEmotions = {};
+const userEmotions = {}; // Stato dell'emoticon per ogni utente
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName, userPeerId) => {
     socket.join(roomId);
-    setTimeout(() => {
-      socket.to(roomId).broadcast.emit("user-connected", userPeerId);
-      socket.emit("user-connected", userPeerId);
 
-      // Invia lo stato dell'emoticon all'utente appena connesso
-      if (userEmotions[userPeerId]) {
-        io.to(userPeerId).emit("user-emotion", userEmotions[userPeerId]);
-      }
-    }, 1000);
+    // Invia lo stato dell'emoticon all'utente appena connesso
+    if (userEmotions[userPeerId]) {
+      io.to(userPeerId).emit("user-emotion", userEmotions[userPeerId]);
+    }
+
+    socket.to(roomId).broadcast.emit("user-connected", userPeerId);
+    socket.emit("user-connected", userPeerId);
 
     socket.on("message", (message) => {
       const emotion = message.emotion;
@@ -56,5 +54,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT || 3030, () => {
-  console.log('Server listening on port', process.env.PORT || 3030);
+  console.log("Server listening on port", process.env.PORT || 3030);
 });
