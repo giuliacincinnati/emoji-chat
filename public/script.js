f1a0a5757d7579598ad7202b9fb1de096e66d20f
giuliacincinnati <cincinnatigiulia@gmail.com>
2023-06-19 15:47:16 +0200

    re

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("/");
+const socket = io("https://video-chat-emoji.herokuapp.com");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 const showChat = document.querySelector("#showChat");
